cb24bfdb1505d9b5caf977a5fcaceae9e5915703
Oscar Lozano <olozano1194@gmail.com>
2026-04-12 03:14:39 -0500

    refactor(routes): agregar rutas de páginas legales al router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { Routes, Route } from "react-router-dom";
 // Page
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
+// Legal Pages
+import TermsPage from "./pages/legal/TermsPage";
+import PrivacyPage from "./pages/legal/PrivacyPage";
+import CookiesPage from "./pages/legal/CookiesPage";
 // Layouts
 import LayoutAdmin from "./layouts/LayoutAdmin";
 // Error
@@ -10,13 +14,15 @@ import Error404 from "./pages/Error404";
 function App() { 
   return (
     <Routes>
-      {/* <Route path="/" element={<Navigate to='/layoutAdmin' />} /> */}
       <Route path="/" element={<LayoutAdmin />}>
         <Route index element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="terminos" element={<TermsPage />} />
+        <Route path="privacidad" element={<PrivacyPage />} />
+        <Route path="cookies" element={<CookiesPage />} />
       </Route>
       <Route path="*" element={<Error404 />} />         
     </Routes>
   )
 }
-export default App
+export default App;
